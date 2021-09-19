e6534540b3ab9a074dc9a075766917a2855b818e
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-09-19 22:54:13 +0600

    try components added

---
+++ try.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const try = () => {
+    return (
+        <div>
+            <h1>Try to be a good code buster.</h1>
+        </div>
+    );
+};
+
+export default try;
